b05faff332d700100639f274fefb959f2b78c1b2
nebyouchaka <nebyouchaka@gmail.com>
2023-11-27 16:10:45 -0500

    update javascript

--- SBA-Html-css-js/javascript/main.js
+++ SBA-Html-css-js/javascript/main.js
@@ -1,74 +1,61 @@
-//const apiUrl = 'https://api.example.com/data'; // Replace with  API endpoint 
-
-// Function to handle login
+// Function to handle the login process
 function login() {
+    // Get user inputs
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
 
-    // Placeholder logic for login
-    if (email && password) {
-        alert("Login successful!");
-        // I can add code to redirect to another page or perform additional actions
-    } else {
-        alert("Invalid login credentials. Please try again.");
+    // Perform basic validation (you may want to enhance this)
+    if (email.trim() === "" || password.trim() === "") {
+        alert("Please enter both email and password.");
+        return;
     }
-}
 
-// Function to validate the contact form
-function validateContactForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-
-    // Placeholder validation logic
-    if (name && email && message) {
-        alert("Contact form submitted successfully!");
-        // I can add AJAX request or other actions here
-        return true; // Form submission allowed
-    } else {
-        alert("Please fill in all fields in the contact form.");
-        return false; // Prevent form submission
-    }
-}
-
-// Example of using a loop
-function iterateOverElements() {
-    var elements = document.querySelectorAll('.example-class');
-
-    for (var i = 0; i < elements.length; i++) {
-        // Do something with each element
-        elements[i].style.color = 'red';
-    }
-}
-
-// Example of using AJAX with vanilla JavaScript
-function fetchDataWithAjax() {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            // Process the data
-            console.log(data);
-        }
-    };
-
-    xhr.open('GET', apiUrl, true);
-    xhr.send();
-}
-
-// Example of using AJAX with jQuery (Optional)
-function fetchDataWithJQuery() {
-    $.ajax({
-        url: apiUrl,
-        method: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            // Process the data
-            console.log(data);
+    // Perform login logic with AJAX request
+    fetch('your_authentication_endpoint', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        error: function (error) {
-            console.log("Error fetching data: " + error);
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
+        return response.json();
+    })
+    .then(data => {
+        // Handle the response from the server
+        if (data.success) {
+            alert("Login successful!");
+            // Redirect or perform additional actions after successful login
+        } else {
+            alert("Login failed. Please check your credentials.");
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert("An error occurred during login. Please try again.");
     });
+}
+function register() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var repassword = document.getElementById("repassword").value;
+
+    if (email.trim() === "" || password.trim() === "" || repassword.trim() === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    if (password !== repassword) {
+        alert("Passwords do not match. Please re-enter your password.");
+        return;
+    }
+
+    // Perform registration logic here (replace this with your actual registration code)
+    alert("Registration successful!");
 }
